52eb793526faa955e568288a2a74f0f5b217fdc4
Animesh Bala Ani <animesh.ani@live.com>
2025-11-04 11:53:11 -0500

    Update on Tmp Storage

--- dify-plugin/README.md
+++ dify-plugin/README.md
@@ -1,17 +1,21 @@
 # Dify API
+
 Reference: https://github.com/langgenius/dify/tree/main/web
 
 #### Remove Previous Leftovers
+
 ```bash
 docker rm -f dify-plugin
 ```
 
 #### Build Image
+
 ```bash
 docker build -t dify-plugin-image:latest -f Dockerfile .
 ```
 
 ### Run Container
+
 ```bash
 docker run -d --name dify-plugin --read-only -v dify-ui-plugin:/tmp --env-file .env -p 5002:5002 dify-plugin-image:latest
 ```
--- dify-plugin/entrypoint.sh
+++ dify-plugin/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -e  # Exit on error
+
 export TIKTOKEN_CACHE_DIR=/tmp/.tiktoken
 
 # Create necessary directories in /tmp (in case they don't exist from volume mount)
 mkdir -p /tmp/.tiktoken /tmp/storage /tmp/logs /tmp/python-packages \
-    /tmp/.cache /tmp/uv-cache /tmp/plugins /tmp/plugins/installed /tmp/plugins/packages
+    /tmp/.cache /tmp/uv-cache /tmp/uv-tools /tmp/uv-python /tmp/pip-cache \
+    /tmp/plugins /tmp/plugins/installed /tmp/plugins/packages \
+    /tmp/persistence /tmp/media-cache
 
 exec /app/main
